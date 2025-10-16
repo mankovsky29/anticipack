fc9a72c7561e47071b775afc9c78a8ff8d191d3d
Siarhei <man.siarhei@outlook.com>
2025-10-16 23:00:31 +0300

    Adding Item styling

--- Anticipack/Anticipack/wwwroot/js/site.js
+++ Anticipack/Anticipack/wwwroot/js/site.js
@@ -1,18 +1,24 @@
-//window.selectAllText = function (el) {
-//    if (!el) return;
-//    // Works for input/textarea
-//    if (typeof el.select === "function") {
-//        el.focus();
-//        el.select();
-//        try {
-//            el.setSelectionRange(0, (el.value ?? "").length);
-//        } catch { /* ignored */ }
-//        return;
-//    }
-//    // Fallback for other elements
-//    const range = document.createRange();
-//    range.selectNodeContents(el);
-//    const sel = window.getSelection();
-//    sel.removeAllRanges();
-//    sel.addRange(range);
-//};
+window.selectAllText = function (el) {
+    if (!el) return;
+    // Works for input/textarea
+    if (typeof el.select === "function") {
+        el.focus();
+        el.select();
+        try {
+            el.setSelectionRange(0, (el.value ?? "").length);
+        } catch { /* ignored */ }
+        return;
+    }
+    // Fallback for other elements
+    const range = document.createRange();
+    range.selectNodeContents(el);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+};
+
+window.focusElement = function (elementRef) {
+    if (elementRef && elementRef.focus) {
+        elementRef.focus();
+    }
+};
